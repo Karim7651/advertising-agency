22333b3036d9906f2cf7062420e7f732df1b0ed4
Karim7651 <karimatef785@gmail.com>
2024-04-04 12:09:40 +0200

    responsiveness

--- src/partial/Header.js
+++ src/partial/Header.js
@@ -5,24 +5,15 @@ import { useTheme } from '../ThemeContext';
 function Header() {
   const darkTheme = useTheme()
   return (
-    <header>
-      
-    <div className={`${darkTheme ? "bgr-gradient":"bgr-gradientLight"}`}>
-      <div className="row justify-content-center text-white">
-        <div className="col-3 offset-1 mt-5 pt-5" id="here" >
-          <h1 className ="headerMainText">BrandName Ads</h1>
-          <div className="headerTextSec">
-          <h2 className="fs-1 headerSecText">Ads that work as hard as you do</h2>
-          <p className="fs-5 headerParagraph">Ready to meet audiences where they shop, read, listen, and watch?</p>
-          </div>
-        </div>
-        <div className="col-6 ">
-          <img  id="headerImage" className='image-fluid float-right' src={headerImage}/>
-        </div>
+    <div className={`${darkTheme? 'headerContainer':'headerContainerLight'}`}>
+      <div className={`${darkTheme ? 'headerText':'headerTextLight'}`}>
+        <h1 id="headerMainText">BrandName ads</h1>
+        <h2 className="headerSecText">Ads that work as hard as you do</h2>
+        <p className="headerSecText">Ready to meet audiences where they shop, read, listen, and watch?</p>
       </div>
-
+      <img src={headerImage} alt="header image" id="headerImage"/> 
     </div>
-    </header>
+
   );
 }
 
